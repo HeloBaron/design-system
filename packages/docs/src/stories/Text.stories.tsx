9277a0de831ca4b25f1d13bb22228b62e0fd0024
HeloBaron <helo.athayde@gmail.com>
2023-07-12 17:57:06 -0300

    feat: Text element

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi, placeat deleniti porro voluptate quis maiores iste totam est fugit nisi aliquam perferendis expedita iusto vitae sapiente modi, pariatur adipisci a.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
