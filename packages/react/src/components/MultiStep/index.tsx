18b948ddefc8c8032fbd4b851182c478cb8d6ccf
HeloBaron <helo.athayde@gmail.com>
2023-07-28 17:11:25 -0300

    feat: MultiStep component

---
+++ packages/react/src/components/MultiStep/index.tsx
@@ -0,0 +1,22 @@
+import { Label, MultiStepConainer, Step, Steps } from './styles'
+
+export interface MultiStepProps {
+  size: number
+  currentStep?: number
+}
+
+export function MultiStep({ size, currentStep = 1 }: MultiStepProps) {
+  return (
+    <MultiStepConainer>
+      <Label>
+        Step {currentStep} of {size}
+      </Label>
+
+      <Steps css={{ '--steps-size': size }}>
+        {Array.from({ length: size }, (_, i) => i + 1).map((step) => {
+          return <Step key={step} active={currentStep >= step} />
+        })}
+      </Steps>
+    </MultiStepConainer>
+  )
+}
